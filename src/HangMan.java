ffa6b102b3881fe1943a5c9cc6870f687da67b81
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2021-01-15 10:40:36 +0100

    a

---
+++ src/HangMan.java
@@ -0,0 +1,8 @@
+public class HangMan {
+    public static void main(String[] args) {
+        String randomword = pickarandomword();
+    }
+    private static String pickarandomword() {
+
+    }
+}
